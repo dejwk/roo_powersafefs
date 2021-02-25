5063d8c6cff642cf8488e429eb1cfb4948f25c3c
Dawid Kurzyniec <dawidk@google.com>
2021-02-25 19:27:47 +0100

    Initial import.

---
+++ roo_powersafefs.cpp
@@ -0,0 +1,178 @@
+#include "roo_powersafefs.h"
+
+namespace roo_powersafefs {
+
+Mount::Mount(Guard* guard, bool forced)
+    : guard_(guard), forced_(forced), mounted_(guard_->tryMount(forced)) {}
+
+Mount::~Mount() {
+  if (mounted_) {
+    guard_->unmount(forced_);
+  }
+}
+
+Mount::Mount(Mount&& other)
+    : guard_(other.guard_), forced_(other.forced_), mounted_(other.mounted_) {
+  other.mounted_ = false;
+}
+
+WriteTransaction::WriteTransaction(Guard* guard, bool forced)
+    : guard_(guard),
+      forced_(forced),
+      active_(guard_->tryBeginWriteTransaction(forced)) {}
+
+WriteTransaction::WriteTransaction(WriteTransaction&& other)
+    : guard_(other.guard_), forced_(other.forced_), active_(other.active_) {
+  other.active_ = false;
+}
+
+WriteTransaction::~WriteTransaction() {
+  if (active_) {
+    guard_->endWriteTransaction(forced_);
+  }
+}
+
+Guard::Guard(Device* device)
+    : device_(device),
+      mode_(FS_NORMAL),
+      mounted_(false),
+      mount_count_(0),
+      forced_mount_count_(0),
+      write_transaction_count_(0),
+      forced_write_transaction_count_(0) {}
+
+Guard::Mode Guard::mode() const {
+  std::lock_guard<std::mutex> guard(mutex_);
+  return mode_;
+}
+
+void Guard::unmountIfPending() {
+  if (mounted_ && mount_count_ == 0) {
+    device_->unmount();
+    mounted_ = false;
+  }
+}
+
+void Guard::setMode(Guard::Mode mode) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  if (mode == mode_) return;
+  switch (mode_) {
+    case FS_EAGER_UNMOUNT: {
+      unmountIfPending();
+      // No break.
+    }
+    case FS_NORMAL: {
+      if (!mounted_ && mount_count_ > 0) {
+        // Need to re-mount.
+        mounted_ = device_->mount();
+      }
+      break;
+    }
+    case FS_LAME_DUCK: {
+      if (!mounted_ && forced_mount_count_ > 0) {
+        // Need to re-mount.
+        mounted_ = device_->mount();
+      }
+      unmountIfPending();
+      break;
+    }
+    case FS_SHUTDOWN: {
+      unmountIfPending();
+      break;
+    }
+    case FS_DISABLED: {
+      if (mounted_) {
+        // Force unmount.
+        device_->unmount();
+        mounted_ = false;
+      }
+      break;
+    }
+  }
+  mode_ = mode;
+}
+
+bool Guard::isMounted() const {
+  std::lock_guard<std::mutex> guard(mutex_);
+  return mounted_;
+}
+
+Mount Guard::mount(bool forced) {
+  return Mount(this, forced);
+}
+
+WriteTransaction Guard::write(bool forced) {
+  return WriteTransaction(this, forced);
+}
+
+int Guard::getPendingMountsCount() const {
+  std::lock_guard<std::mutex> guard(mutex_);
+  return mount_count_;
+}
+
+int Guard::getPendingWriteTransactionsCount() const {
+  std::lock_guard<std::mutex> guard(mutex_);
+  return write_transaction_count_;
+}
+
+bool Guard::tryMount(bool forced) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  switch (mode_) {
+    case FS_DISABLED:
+    case FS_SHUTDOWN: {
+      return false;
+    }
+    case FS_LAME_DUCK: {
+      if (!forced) return false;
+    }
+    case FS_NORMAL:
+    case FS_EAGER_UNMOUNT: {
+    }
+  }
+  if (!mounted_) {
+    mounted_ = device_->mount();
+  }
+  if (mounted_) {
+    ++mount_count_;
+    if (forced) ++forced_mount_count_;
+  }
+  return mounted_;
+}
+
+void Guard::unmount(bool forced) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  --mount_count_;
+  if (forced) --forced_mount_count_;
+  if (mounted_ && mount_count_ == 0 && mode_ != FS_NORMAL) {
+    device_->unmount();
+    mounted_ = false;
+  }
+}
+
+bool Guard::tryBeginWriteTransaction(bool forced) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  if (!mounted_) return false;
+  switch (mode_) {
+    case FS_DISABLED:
+    case FS_SHUTDOWN: {
+      return false;
+    }
+    case FS_LAME_DUCK: {
+      if (!forced) return false;
+    }
+    case FS_NORMAL:
+    case FS_EAGER_UNMOUNT: {
+    }
+  }
+  ++write_transaction_count_;
+  if (forced) ++forced_write_transaction_count_;
+  return true;
+}
+
+void Guard::endWriteTransaction(bool forced) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  --write_transaction_count_;
+  if (forced) --forced_write_transaction_count_;
+}
+
+}  // namespace roo_powersafefs
---
+++ roo_powersafefs.h
@@ -0,0 +1,143 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+
+namespace roo_powersafefs {
+
+class Device {
+ public:
+  virtual ~Device() {}
+  virtual bool mount() = 0;
+  virtual void unmount() = 0;
+};
+
+class Guard;
+
+// Created in order to request that the filesystem is mounted. The filesystem
+// will remain mounted for as long as this object is alive. If the guard
+// has been configured for delayed unmount, the filesystem may remain =
+// mounted after the object is destroyed.
+// If you need write access to the filesystem, also use WriteTransaction
+// (see below).
+// Typical usage scenario:
+//
+// Mount mount(&guard);
+// if (mount.mounted()) {
+//   // Perform reads on the filesystem.
+// }
+class Mount {
+ public:
+  Mount(Guard* guard, bool forced = false);
+  Mount(Mount&& other);
+
+  bool mounted() const { return mounted_; }
+
+  ~Mount();
+
+ private:
+  friend class Guard;
+
+  Mount(const Mount&) = delete;
+  Mount& operator=(const Mount&) = delete;
+
+  Guard* guard_;
+  bool forced_;
+  bool mounted_;
+};
+
+// Created in order to signal intent to perform a write operation on the
+// file system. The typical usage scenario:
+//
+// Mount mount(&guard);
+// if (mount.mounted()) {
+//   WriteTransaction write(&guard);
+//   if (write.active()) { ... }
+// }
+class WriteTransaction {
+ public:
+  WriteTransaction(Guard* guard, bool forced = false);
+  WriteTransaction(WriteTransaction&& other);
+
+  ~WriteTransaction();
+
+  bool active() const { return active_; }
+
+ private:
+  WriteTransaction(const WriteTransaction&) = delete;
+  WriteTransaction& operator=(const WriteTransaction&) = delete;
+
+  Guard* guard_;
+  bool forced_;
+  bool active_;
+};
+
+class Guard {
+ public:
+  enum Mode {
+    // All mount and write transaction operations are granted.
+    // The filesystem gets mounted when requested. Once mounted, the
+    // filesystem remains mounted indefinitely.
+    FS_NORMAL,
+
+    // Like FS_NORMAL, except that the filesystem gets
+    // unmounted as soon as all mount objects are destroyed.
+    FS_EAGER_UNMOUNT,
+
+    // New mount and write transaction requests are rejected,
+    // unless they are called with the 'force' mode. The filesystem gets
+    // unmounted as soon as all mount objects are destroyed.
+    FS_LAME_DUCK,
+
+    // New mount and write transaction requests are rejected,
+    // even if they are called with the 'force' mode. The filesystem gets
+    // unmounted as soon as all mount objects are destroyed.
+    FS_SHUTDOWN,
+
+    // The filesystem gets immediately unmounted, even if it is in use.
+    FS_DISABLED
+  };
+
+  Guard(Device* device);
+
+  Mode mode() const;
+  void setMode(Mode);
+
+  Mount mount(bool force = false);
+  WriteTransaction write(bool force = false);
+
+  // Returns true if the underlying device is mounted; false otherwise.
+  // In FS_DISABLED mode, will always return true. In FS_NORMAL, may return
+  // true even if getPendingMountsCount() is zero.
+  bool isMounted() const;
+
+  // Returns the number of Mount objects for this guard object.
+  int getPendingMountsCount() const;
+
+  // Returns the number of write transactions for this guard object.
+  int getPendingWriteTransactionsCount() const;
+
+ private:
+  friend class Mount;
+  friend class WriteTransaction;
+
+  bool tryMount(bool forced);
+  void unmount(bool forced);
+
+  bool tryBeginWriteTransaction(bool forced);
+  void endWriteTransaction(bool forced);
+
+  void unmountIfPending();
+  
+  Device* device_;
+  mutable std::mutex mutex_;
+
+  Mode mode_;
+  bool mounted_;
+  int mount_count_;
+  int forced_mount_count_;
+  int write_transaction_count_;
+  int forced_write_transaction_count_;
+};
+
+}  // namespace roo_powersafefs
